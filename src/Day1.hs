564d6f1fb87298bef2419ca0e045a2d57570bfa6
alex800121 <alex800121@hotmail.com>
2024-12-01 13:33:07 +0800

    day1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,4 +1,24 @@
 module Day1 where
 
+import Data.List (sort, transpose, group)
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as IM
+import Data.Maybe (fromMaybe)
+
 day1 :: IO ()
-day1 = return ()
+day1 = do
+  [a, b] <- map sort . transpose . map (map (read @Int) . words) . lines <$> readFile "input/input1.txt"
+  putStrLn
+    . ("day1: " ++)
+    . show
+    . sum
+    . map abs
+    $ zipWith subtract a b
+  let f = IM.fromList . map ((,) <$> head <*> length) . group
+      a' = f a
+      b' = f b
+  putStrLn
+    . ("day1: " ++)
+    . show
+    $ IM.foldrWithKey (\k v acc -> fromMaybe 0 (b' IM.!? k) * v * k + acc) 0 a'
+
