9e6736da6b877dda614f9899ad3b880fec0d7acf
alex800121 <alex800121@hotmail.com>
2024-12-04 21:49:34 +0800

    day4

--- src/Day4.hs
+++ src/Day4.hs
@@ -1,4 +1,50 @@
 module Day4 where
 
+import Data.Array.Unboxed (UArray, indices, (!?))
+import Data.Bifunctor (Bifunctor (bimap, first))
+import MyLib (drawArray)
+
+type Index = (Int, Int)
+
+testXMAS2 :: [(Index, Char)] -> Index -> UArray Index Char -> Bool
+testXMAS2 [] _ _ = True
+testXMAS2 ((i, c) : xs) xy@(x, y) arr = Just c == (arr !? bimap (+ x) (+ y) i) && testXMAS2 xs xy arr
+
+directions = [(x, y) | x <- [-1 .. 1], y <- [-1 .. 1], not (x == 0 && y == 0)]
+
+rotate :: Index -> Index
+rotate (x, y) = (y, -x)
+
+xmas1 = map (\(x, y) -> (`zip` "XMAS") . take 4 $ iterate (bimap (+ x) (+ y)) (0, 0)) directions
+
+xmas2 =
+  take 4 $
+    iterate
+      (map (first rotate))
+      [ ((0, 0), 'A'),
+        ((-1, -1), 'M'),
+        ((-1, 1), 'M'),
+        ((1, -1), 'S'),
+        ((1, 1), 'S')
+      ]
+
+solve input x =
+  length
+    [ ()
+      | i <- indices input,
+        xmas <- x,
+        testXMAS2 xmas i input
+    ]
+
 day4 :: IO ()
-day4 = return ()
+day4 = do
+  -- input <- drawArray @UArray . lines <$> readFile "input/test4.txt"
+  input <- drawArray @UArray . lines <$> readFile "input/input4.txt"
+  putStrLn
+    . ("day4a: " ++)
+    . show
+    $ solve input xmas1
+  putStrLn
+    . ("day4b: " ++)
+    . show
+    $ solve input xmas2
