c05c47a6fc0806a3a9cbc02edcea9d731f02457c
alex800121 <alex800121@hotmail.com>
2024-12-07 16:50:53 +0800

    day6-7

--- src/Day7.hs
+++ src/Day7.hs
@@ -1,4 +1,45 @@
 module Day7 where
 
+import Control.Monad (unless)
+import Control.Monad.Hefty
+import Control.Monad.Hefty.NonDet
+import Data.List.Split (splitOn)
+import Data.Maybe (isJust, mapMaybe, catMaybes, fromMaybe)
+import Paths_AOC2024 (getDataDir)
+import Text.Read (readMaybe)
+import Data.Functor (($>))
+import HeftiaParser (runNonDetMaybe)
+
+inputParser :: String -> Maybe (Int, [Int])
+inputParser s = do
+  let [a, b] = splitOn ": " s
+  (,) <$> readMaybe @Int a <*> pure (map read $ words b)
+
+tryOperators :: forall ef eh. (Choose <| ef, Empty <| ef) => [Int -> Int -> Int] -> (Int, [Int]) -> Eff eh ef Int
+tryOperators _ (_, []) = empty
+tryOperators ops (target, x : xs) = go xs x
+  where
+    go ys current
+      | [] <- ys = if target == current then pure target else empty
+      -- | [] <- ys = unless (target == currentInt) empty
+      | (x : xs) <- ys, target < current = empty
+      | (x : xs) <- ys = do
+          op <- choice ops
+          go xs (current `op` x)
+
+con a b = b + (a * (10 ^ (floor (logBase 10 (fromIntegral b)) + 1)))
+
 day7 :: IO ()
-day7 = return ()
+day7 = do
+  -- input <- mapMaybe inputParser . lines <$> (readFile . (++ "/input/test7.txt") =<< getDataDir)
+  input <- mapMaybe inputParser . lines <$> (readFile . (++ "/input/input7.txt") =<< getDataDir)
+  putStrLn
+    . ("day7a: " ++)
+    . show
+    . sum
+    $ mapMaybe (\x -> (runPure . runNonDetMaybe . tryOperators [(+), (*)]) x $> fst x)  input
+  putStrLn
+    . ("day7b: " ++)
+    . show
+    . sum
+    $ mapMaybe (\x -> (runPure . runNonDetMaybe . tryOperators [(+), (*), con]) x $> fst x)  input
