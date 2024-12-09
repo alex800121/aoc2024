c5f6bfe69023841e9ae8c74708a56a79f140493f
alex800121 <alex800121@hotmail.com>
2024-12-09 19:20:31 +0800

    day9

--- src/Day6.hs
+++ src/Day6.hs
@@ -2,8 +2,8 @@ module Day6 where
 
 import Control.Monad.ST (ST, runST)
 import Data.Array (Array)
-import Data.Array.IArray
-import Data.Array.ST (MArray (newArray, newArray_), STArray, freeze, runSTArray, writeArray)
+import Data.Array.IArray ( Ix(inRange), Array, (!?), IArray(bounds), elems )
+import Data.Array.ST (MArray (newArray, newArray_), STArray, freeze, runSTArray, writeArray, getElems, modifyArray')
 import Data.Bifunctor (Bifunctor (..))
 import Data.Bits (Bits (..))
 import Data.List (elemIndex, find, findIndex)
@@ -15,6 +15,7 @@ import Data.Set qualified as Set
 import Data.Word (Word8)
 import MyLib (Direction (..), Parser, drawArray, toIndex)
 import Paths_AOC2024 (getDataDir)
+import GHC.Arr (newSTArray)
 
 type Index = (Int, Int)
 
@@ -23,34 +24,33 @@ type Guard = (Index, Direction)
 -- data Cell = Space | Obstacle | Stepped Word8 deriving (Show, Eq, Ord)
 data Cell = Space | Obstacle deriving (Show, Eq, Ord)
 
-type A = Map Index Cell
-
+type A = Array Index Cell
 inputParser :: String -> (Guard, A)
-inputParser s = go Map.empty undefined 0 0 x xs
+inputParser s = runST $ do
+  arr <- newArray ((0, 0), (length x - 1, length xs)) Space :: ST s (STArray s Index Cell)
+  (,) <$> go arr undefined 0 0 x xs <*> freeze arr
   where
     x : xs = lines s
-    go arr g x y [] [] = (g, arr)
+    go arr g x y [] [] = pure g
     go arr g x y [] (l : xs) = go arr g 0 (succ y) l xs
     go arr g x y (c : ys) xs = case c of
-      '#' -> go arr0 g (succ x) y ys xs
-      'v' -> go arr1 ((x, y), South) (succ x) y ys xs
-      '^' -> go arr1 ((x, y), North) (succ x) y ys xs
-      '<' -> go arr1 ((x, y), West) (succ x) y ys xs
-      '>' -> go arr1 ((x, y), East) (succ x) y ys xs
-      _ -> go arr1 g (succ x) y ys xs
-      where
-        arr0 = Map.insert (x, y) Obstacle arr
-        arr1 = Map.insert (x, y) Space arr
+      '#' -> writeArray arr (x, y) Obstacle >> go arr g (succ x) y ys xs
+      'v' -> go arr ((x, y), South) (succ x) y ys xs
+      '^' -> go arr ((x, y), North) (succ x) y ys xs
+      '<' -> go arr ((x, y), West) (succ x) y ys xs
+      '>' -> go arr ((x, y), East) (succ x) y ys xs
+      _ -> go arr g (succ x) y ys xs
 
 walk :: A -> Guard -> Maybe (Int, Int)
 walk m = go Nothing (0, Map.empty)
   where
+    testRange = inRange (bounds m)
     go testing (n, s) (i, d)
       -- repeated
       | maybe False (flip (testBit @Word8) (fromEnum d)) (s Map.!? i) = Nothing
       -- step out of bound -> return Just (if testing -> undefined; else -> answer)
       | Just (iTurn, d') <- next,
-        iTurn `Map.notMember` m =
+        not (testRange iTurn) =
           Just (if isJust testing then (undefined, undefined) else (n, Map.size s'))
       -- in bound, not testing, next cell not travelled -> test next cell and continue
       | Just (iTurn, d') <- next,
@@ -61,27 +61,28 @@ walk m = go Nothing (0, Map.empty)
       where
         s' = Map.insertWith (.|.) i (bit $ fromEnum d) s
         next =
-          find (\(i, _) -> Just Obstacle /= m Map.!? i && Just i /= testing)
+          find (\(i, _) -> Just Obstacle /= m !? i && Just i /= testing)
             . map (\d -> (bimap (+ fst i) (+ snd i) (toIndex d), d))
             . take 4
             $ iterate succ d
 
--- type A = Array Index Cell
+-- type A = Map Index Cell
 -- inputParser :: String -> (Guard, A)
--- inputParser s = runST $ do
---   arr <- newArray ((0, 0), (length x - 1, length xs)) Space :: ST s (STArray s Index Cell)
---   (,) <$> go arr undefined 0 0 x xs <*> freeze arr
+-- inputParser s = go Map.empty undefined 0 0 x xs
 --   where
 --     x : xs = lines s
---     go arr g x y [] [] = pure g
+--     go arr g x y [] [] = (g, arr)
 --     go arr g x y [] (l : xs) = go arr g 0 (succ y) l xs
 --     go arr g x y (c : ys) xs = case c of
---       '#' -> writeArray arr (x, y) Obstacle >> go arr g (succ x) y ys xs
---       'v' -> go arr ((x, y), South) (succ x) y ys xs
---       '^' -> go arr ((x, y), North) (succ x) y ys xs
---       '<' -> go arr ((x, y), West) (succ x) y ys xs
---       '>' -> go arr ((x, y), East) (succ x) y ys xs
---       _ -> go arr g (succ x) y ys xs
+--       '#' -> go arr0 g (succ x) y ys xs
+--       'v' -> go arr1 ((x, y), South) (succ x) y ys xs
+--       '^' -> go arr1 ((x, y), North) (succ x) y ys xs
+--       '<' -> go arr1 ((x, y), West) (succ x) y ys xs
+--       '>' -> go arr1 ((x, y), East) (succ x) y ys xs
+--       _ -> go arr1 g (succ x) y ys xs
+--       where
+--         arr0 = Map.insert (x, y) Obstacle arr
+--         arr1 = Map.insert (x, y) Space arr
 -- walk :: A -> Guard -> Maybe (Int, Int)
 -- walk m = go Nothing (0, Map.empty)
 --   where
@@ -91,7 +92,7 @@ walk m = go Nothing (0, Map.empty)
 --       | maybe False (`testBit` fromEnum d) (s Map.!? i) = Nothing
 --       -- step out of bound -> return Just (if testing -> undefined; else -> answer)
 --       | Just (iTurn, d') <- next,
---         not (inRange (bounds m) iTurn) =
+--         iTurn `Map.notMember` m =
 --           Just (if isJust testing then (undefined, undefined) else (n, Map.size s'))
 --       -- in bound, not testing, next cell not travelled -> test next cell and continue
 --       | Just (iTurn, d') <- next,
@@ -102,7 +103,7 @@ walk m = go Nothing (0, Map.empty)
 --       where
 --         s' = Map.insertWith (.|.) i (bit $ fromEnum d) s
 --         next =
---           find (\(i, _) -> Just Obstacle /= m !? i && Just i /= testing)
+--           find (\(i, _) -> Just Obstacle /= m Map.!? i && Just i /= testing)
 --             . map (\d -> (bimap (+ fst i) (+ snd i) (toIndex d), d))
 --             . take 4
 --             $ iterate succ d
--- src/Day9.hs
+++ src/Day9.hs
@@ -1,4 +1,97 @@
 module Day9 where
 
+import Control.Arrow ((>>>))
+import Control.Monad (when, (>=>))
+import Control.Monad.ST.Strict (ST, fixST, runST)
+import Data.Char (digitToInt, isDigit)
+import Data.Function (on)
+import Data.IntMap (IntMap)
+import Data.IntMap qualified as IM
+import Data.IntSet (IntSet)
+import Data.IntSet qualified as IS
+import Data.List (sort, sortBy)
+import Data.Maybe (mapMaybe)
+import Data.Ord (Down (..), comparing)
+import Data.Vector (Vector, freeze, ifoldl', takeWhile, thaw)
+import Data.Vector.Mutable (MVector, STVector, generate, new, read, write)
+import Debug.Trace (traceShow)
+import Paths_AOC2024 (getDataDir)
+
+data Disk = Space | ID Int deriving (Show, Eq)
+
+readInputB = go 0 0 (IM.empty, IM.empty)
+  where
+    go i pos (s, t) (x : xs) | isDigit x = go' i (pos + digitToInt x) (IM.insert i (pos, digitToInt x) s, t) xs
+    go _ _ s _ = s
+    go' i pos (s, t) (x : xs) | isDigit x = go (i + 1) (pos + digitToInt x) (s, IM.insertWith (<>) (digitToInt x) (IS.singleton pos) t) xs
+    go' _ _ s _ = s
+
+condenseB :: (IntMap (Int, Int), IntMap IntSet) -> (IntMap (Int, Int), IntMap IntSet)
+condenseB (s, t) = go li (s, t)
+  where
+    li = sortBy (comparing Down) (IM.keys s)
+    go (x : xs) (s, t)
+      -- \| traceShow (s, t) False = undefined
+      | Just (oldPos, len) <- s IM.!? x,
+        ((newPos, rest), len') : _ <- sortBy (compare `on` fst . fst) $ mapMaybe (liftA2 (,) <$> ((t IM.!?) >=> IS.minView) <*> pure) [len .. 9],
+        -- newLen <- len' - len = traceShow (x, newLen, newPos, len) $
+        newLen <- len' - len,
+        newPos < oldPos =
+          go xs (IM.insert x (newPos, len) s, IM.insertWith (<>) newLen (IS.singleton (newPos + len)) $ IM.insert len' rest t)
+      | otherwise = go xs (s, t)
+    go _ o = o
+
+readInputA s = runST $ do
+  output <- generate l (const Space) :: ST s (STVector s Disk)
+  f 0 0 output xs
+  freeze output
+  where
+    xs = map digitToInt $ init s
+    l = sum xs
+    f _ _ _ [] = pure ()
+    f i n o (y : ys) = mapM_ (\j -> write o j (ID i)) [n .. (n + y - 1)] >> g i (n + y) o ys
+    g _ _ _ [] = pure ()
+    g i n o (y : ys) = f (i + 1) (n + y) o ys
+
+condenseA v = runST $ do
+  mv <- thaw v :: ST s (STVector s Disk)
+  go 0 (length v - 1) mv
+  freeze mv
+  where
+    go :: Int -> Int -> STVector s Disk -> ST s ()
+    go i j k = do
+      i' <- f i
+      (j', x) <- g j
+      when (i' < j') $ do
+        write k i' x
+        write k j' Space
+        go i' j' k
+      where
+        f n = Data.Vector.Mutable.read k n >>= \x -> if x == Space then pure n else f (n + 1)
+        g n = Data.Vector.Mutable.read k n >>= \x -> if x /= Space then pure (n, x) else g (n - 1)
+
+checkSumA = ifoldl' f 0
+  where
+    f acc _ Space = acc
+    f acc i (ID x) = acc + (i * x)
+
+checkSumB :: IntMap (Int, Int) -> Int
+checkSumB = IM.foldlWithKey (\acc k (pos, len) -> acc + ((((pos + (pos + len - 1)) * len) `div` 2) * k)) 0
+
 day9 :: IO ()
-day9 = return ()
+day9 = do
+  input <- readFile . (++ "/input/input9.txt") =<< getDataDir
+  -- input <- readFile . (++ "/input/test9.txt") =<< getDataDir
+  let inputA = readInputA input
+      inputB = readInputB input
+  putStrLn
+    . ("Day9a: " ++)
+    . show
+    . checkSumA
+    $ condenseA inputA
+  putStrLn
+    . ("Day9b: " ++)
+    . show
+    . checkSumB
+    . fst
+    $ condenseB inputB
