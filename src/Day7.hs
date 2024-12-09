67e05533b97305396d7491cd7ac4e9de23c6666d
alex800121 <alex800121@hotmail.com>
2024-12-09 19:20:31 +0800

    day9

--- src/Day7.hs
+++ src/Day7.hs
@@ -1,8 +1,8 @@
 module Day7 where
 
 import Control.Monad (unless)
-import Control.Monad.Hefty
-import Control.Monad.Hefty.NonDet
+-- import Control.Monad.Hefty
+-- import Control.Monad.Hefty.NonDet
 import Data.List.Split (splitOn)
 import Data.Maybe (isJust, mapMaybe, catMaybes, fromMaybe)
 import Paths_AOC2024 (getDataDir)
@@ -11,23 +11,19 @@ import Data.Functor (($>))
 import HeftiaParser (runNonDetMaybe)
 import Data.List (partition)
 import Control.Parallel.Strategies (rpar, parMap)
+import Control.Applicative
 
 inputParser :: String -> Maybe (Int, [Int])
 inputParser s = do
   let [a, b] = splitOn ": " s
   (,) <$> readMaybe @Int a <*> pure (map read $ words b)
 
-tryOperators :: forall ef eh. (Choose <| ef, Empty <| ef) => [Int -> Int -> Int] -> (Int, [Int]) -> Eff eh ef Int
-tryOperators _ (_, []) = empty
+tryOperators :: [Int -> Int -> Int] -> (Int, [Int]) -> Bool
+tryOperators _ (_, []) = False
 tryOperators ops (target, x : xs) = go xs x
   where
-    go ys current
-      | [] <- ys = if target == current then pure target else empty
-      -- | [] <- ys = unless (target == currentInt) empty
-      | (x : xs) <- ys, target < current = empty
-      | (x : xs) <- ys = do
-          op <- choice ops
-          go xs (current `op` x)
+    go [] current = target == current
+    go (x : xs) current = target >= current && any (\op -> go xs (current `op` x)) ops
 
 con a b = b + (a * (10 ^ (floor (logBase 10 (fromIntegral b)) + 1)))
 
@@ -40,10 +36,10 @@ day7 = do
     . show
     . sum
     . map fst
-    $ filter (isJust . runPure . runNonDetMaybe . tryOperators [(+), (*)])  input
+    $ filter (tryOperators [(+), (*)]) input
   putStrLn
     . ("day7b: " ++)
     . show
     . sum
     . map fst
-    $ filter (isJust . runPure . runNonDetMaybe . tryOperators [(+), (*), con])  input
+    $ filter (tryOperators [(+), (*), con]) input
