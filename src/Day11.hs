b6a33688eb5fa9d5ce2162801d197e2f40046b21
alex800121 <alex800121@hotmail.com>
2024-12-11 14:42:12 +0800

    day11

--- src/Day11.hs
+++ src/Day11.hs
@@ -1,4 +1,35 @@
 module Day11 where
 
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Debug.Trace (traceShow)
+import Paths_AOC2024 (getDataDir)
+
+log10 b | b <= 0 = 1
+log10 b = floor (logBase 10 (fromIntegral b)) + 1
+
+step i
+  -- \| traceShow (i, l) False = undefined
+  | i == 0 = [1]
+  | even l = [i `div` l2, i `mod` l2]
+  | otherwise = [i * 2024]
+  where
+    l = log10 i
+    l2 = 10 ^ (l `div` 2)
+
+blink :: Map Integer Int -> Map Integer Int
+blink = Map.foldlWithKey' (\acc k e -> Map.unionsWith (+) (acc : map (`Map.singleton` e) (step k))) Map.empty
+
 day11 :: IO ()
-day11 = return ()
+day11 = do
+  input <- Map.unionsWith (+) . map (uncurry Map.singleton . (,1) . read @Integer) . words <$> (readFile . (++ "/input/input11.txt") =<< getDataDir)
+  putStrLn
+    . ("day11a: " ++)
+    . show
+    . sum
+    $ iterate blink input!! 25
+  putStrLn
+    . ("day11b: " ++)
+    . show
+    . sum
+    $ iterate blink input !! 75
