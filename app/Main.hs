b4c291b2935abe5a331e31d63e440b7044bd1a88
alex800121 <alex800121@hotmail.com>
2024-12-14 07:39:50 +0800

    day13 part1

--- app/Main.hs
+++ app/Main.hs
@@ -29,18 +29,18 @@ import Day9
 
 main :: IO ()
 main = do
-  day1
-  day2
-  day3
-  day4
-  day5
-  day6
-  day7
-  day8
-  day9
-  day10
-  day11
-  day12
+  -- day1
+  -- day2
+  -- day3
+  -- day4
+  -- day5
+  -- day6
+  -- day7
+  -- day8
+  -- day9
+  -- day10
+  -- day11
+  -- day12
   day13
   day14
   day15
