56c71e63e2fb4d23000fb9ff7104ffffd02e496a
alex800121 <alex800121@hotmail.com>
2024-12-19 06:38:18 +0800

    day17

--- src/Day16.hs
+++ src/Day16.hs
@@ -1,4 +1,111 @@
 module Day16 where
 
+import Control.Monad.Hefty
+import Control.Monad.Hefty.State (State, modify, runState)
+import Data.Bifunctor (Bifunctor (..))
+import Data.List (partition)
+import Data.Map.Strict (Map)
+import Data.Map.Strict qualified as Map
+import Data.Maybe (listToMaybe, mapMaybe)
+import Data.PQueue.Prio.Min (MinPQueue (..))
+import Data.PQueue.Prio.Min qualified as Q
+import Data.Set (Set)
+import Data.Set qualified as Set
+import Debug.Trace (traceShow)
+import MyLib
+  ( Direction (..),
+    drawArray,
+    drawGraph,
+    drawMap,
+    drawMapWithKeyM,
+    toIndex,
+  )
+import Paths_AOC2024 (getDataDir)
+
+type M = Set Index
+
+type MPlus = Map Index [(Int, (Direction, Index))]
+
+type Index = (Int, Int)
+
+dijkstra :: (Index -> Bool) -> MPlus -> (Direction, Index) -> [(Int, [Index])]
+dijkstra end mplus start = go Map.empty maxBound (Q.singleton 0 [start])
+  where
+    go _ _ Empty = []
+    go travelled max ((n, r@((d, i) : rest)) :< q)
+      | max < n = []
+      | end i = (n, map snd r) : go travelled' n q
+      | maybe False (< n) (travelled Map.!? (d, i)) = go travelled max q
+      | otherwise = go travelled' max q'
+      where
+        travelled' = Map.insertWith min (d, i) n travelled
+        q' = q <> Q.fromList (map (second (: r)) l)
+        l = mapMaybe f (mplus Map.! i)
+        f (n', (d', i'))
+          | d' `elem` [succ d, pred d] = Just (n' + n + 1000, (d', i'))
+          | d' == succ (succ d) = Nothing
+          | d' == d = Just (n' + n, (d', i'))
+
+flood :: [Index] -> M -> Index -> MPlus
+flood int m i = go Map.empty Set.empty [i]
+  where
+    go acc _ [] = acc
+    go acc travelled (x : xs)
+      | x `Set.member` travelled = go acc travelled xs
+      | otherwise = go acc' travelled' (xs' <> xs)
+      where
+        next = floodOnce int m x
+        acc' = Map.insertWith (<>) x next acc
+        travelled' = Set.insert x travelled
+        xs' = map (snd . snd) next
+
+floodOnce :: [Index] -> M -> Index -> [(Int, (Direction, Index))]
+floodOnce int m i = go [] 1 is
+  where
+    is = filter ((`Set.member` m) . snd) $ map ((,) <$> id <*> bimap (+ fst i) (+ snd i) . toIndex) [minBound .. maxBound]
+    go acc n start
+      | null start = acc
+      | otherwise = go acc' (n + 1) start''
+      where
+        f (d, i) = filter ((`Set.member` m) . snd) [(d', i') | d' <- [pred d, succ d], let i' = bimap (+ fst i) (+ snd i) (toIndex d')]
+        (intersections, start') = partition ((||) <$> (`elem` int) . snd <*> not . null . f) start
+        acc' = acc <> map (n,) intersections
+        start'' = filter ((`Set.member` m) . snd) $ map (\(d, i) -> (d, bimap (+ fst i) (+ snd i) (toIndex d))) start'
+
+readInput :: String -> ((Index, Index), M)
+readInput s = second Map.keysSet $ runPure $ runState ((0, 0), (0, 0)) $ drawMapWithKeyM f (0, 0) s
+  where
+    f (_, y) '\n' = pure ((0, y + 1), Nothing)
+    f (x, y) '.' = pure ((x + 1, y), Just ())
+    f (x, y) 'S' = modify @(Index, Index) (first (const (x, y))) >> pure ((x + 1, y), Just ())
+    f (x, y) 'E' = modify @(Index, Index) (second (const (x, y))) >> pure ((x + 1, y), Just ())
+    f (x, y) _ = pure ((x + 1, y), Nothing)
+
+buildPoints :: [Index] -> [Index]
+buildPoints [] = []
+buildPoints [x] = [x]
+buildPoints ((x0, y0) : x@(x1, y1) : xs) = [(x, y) | x <- [xmin .. xmax], y <- [ymin .. ymax]] <> buildPoints (x : xs)
+  where
+    xmin = min x0 x1
+    xmax = max x0 x1
+    ymin = min y0 y1
+    ymax = max y0 y1
+
 day16 :: IO ()
-day16 = return ()
+day16 = do
+  ((start, end), input) <- readInput <$> (readFile . (++ "/input/input16.txt") =<< getDataDir)
+  -- ((start, end), input) <- readInput <$> (readFile . (++ "/input/test16.txt") =<< getDataDir)
+  -- ((start, end), input) <- readInput <$> (readFile . (++ "/input/test16'.txt") =<< getDataDir)
+  let mplus = flood [start, end] input start
+      ans = dijkstra (== end) mplus (East, start)
+  putStrLn
+    . ("day16a: " ++)
+    . show
+    . fmap fst
+    $ listToMaybe ans
+  putStrLn
+    . ("day16b: " ++)
+    . show
+    . Set.size
+    . Set.fromList
+    $ concatMap (buildPoints . snd) ans
--- src/Day17.hs
+++ src/Day17.hs
@@ -1,4 +1,99 @@
 module Day17 where
 
+import Control.Monad (guard)
+import Data.Bits (Bits (..))
+import Data.Vector.Mutable qualified as M
+import Data.Vector.Strict qualified as U
+import Debug.Trace (traceM, traceShow)
+import MyLib (Parser)
+import Paths_AOC2024 (getDataDir)
+import Data.List (intercalate)
+
+-- test:
+-- Register A: 2024
+-- Register B: 0
+-- Register C: 0
+--
+-- Program: 0,3,5,4,3,0
+ins' :: Ins
+ins' = U.fromList [0, 3, 5, 4, 3, 0]
+
+input' = G 0 (R 2024 0 0) []
+
+-- input:
+-- Register A: 33024962
+-- Register B: 0
+-- Register C: 0
+--
+-- Program: 2,4,1,3,7,5,1,5,0,3,4,2,5,5,3,0
+{--
+ - a = x, b = 0, c = 0
+ - 0. 2 4 => b <- a and b111 => b <- x and b111
+ - 2. 1 3 => b <- b xor b011 => b <- (x and b111) xor b011
+ - 4. 7 5 => c <- a shiftR b => c <- x shiftR ((x and b111) xor b011)
+ - 6. 1 5 => b <- b xor b101 => b <- ((x and b111) xor b011) xor b101
+ - 8. 0 3 => a <- a shiftR 3 => a <- x shiftR 3
+ - 10. 4 2 => b <- b xor c => b <- (((x and b111) xor b011) xor b101) xor (x shiftR ((x and b111) xor b011))
+ - 12. 5 5 => b and b111 => output b
+ - 14. 3 0 => if a == 0 then end, else a = x shiftR 3 and restart
+ --}
+ins :: Ins
+ins = U.fromList [2, 4, 1, 3, 7, 5, 1, 5, 0, 3, 4, 2, 5, 5, 3, 0]
+
+input = G 0 (R 33024962 0 0) []
+
+type Ins = U.Vector Integer
+
+data GameState a = G {_pos :: Int, _reg :: Reg a, _out :: [a]}
+  deriving (Show)
+
+data Reg a = R {_a :: a, _b :: a, _c :: a}
+  deriving (Show)
+
+solveB :: Ins -> [Integer]
+solveB ins = go (reverse $ U.toList ins) 0
+  where
+    go [] a = pure a
+    go (n : ns) a = do
+      c <- [0 .. 7]
+      let a8 = a * 8 + c
+      guard $ n == last (_out (run ins (G 0 (R a8 0 0) [])))
+      go ns a8
+
+step :: Ins -> GameState Integer -> Maybe (GameState Integer)
+step ins (G pos reg out) = f <$> ((,) <$> (ins U.!? pos) <*> (ins U.!? (pos + 1)))
+  where
+    combo a | a >= 0 && a <= 3 = a
+    combo 4 = _a reg
+    combo 5 = _b reg
+    combo 6 = _c reg
+    combo a = error $ show a ++ " is not a valid operand"
+    f (0, c) = G (pos + 2) (reg {_a = _a reg `shiftR` fromIntegral (combo c)}) out
+    f (1, c) = G (pos + 2) (reg {_b = _b reg `xor` c}) out
+    f (2, c) = G (pos + 2) (reg {_b = combo c .&. 7}) out
+    f (3, c)
+      | _a reg == 0 = G (pos + 2) reg out
+      | otherwise = G (fromIntegral c) reg out
+    f (4, c) = G (pos + 2) (reg {_b = _b reg `xor` _c reg}) out
+    f (5, c) = G (pos + 2) reg (o : out)
+      where
+        o = combo c .&. 7
+    f (6, c) = G (pos + 2) (reg {_b = _a reg `shiftR` fromIntegral (combo c)}) out
+    f (7, c) = G (pos + 2) (reg {_c = _a reg `shiftR` fromIntegral (combo c)}) out
+
+run ins g = maybe g (run ins) (step ins g)
+
 day17 :: IO ()
-day17 = return ()
+day17 = do
+  putStrLn
+    . ("day17a: " ++)
+    . intercalate ","
+    . map show
+    . reverse
+    . _out 
+    $ run ins input
+  putStrLn
+    . ("day17b: " ++)
+    . show
+    . minimum
+    $ solveB ins
