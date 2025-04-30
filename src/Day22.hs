4d1ad7b2e40318827a463c73fda588af274c882a
alex800121 <alex800121@hotmail.com>
2025-04-30 11:03:25 +0300

    faster day22

--- src/Day22.hs
+++ src/Day22.hs
@@ -1,58 +1,46 @@
 {-# LANGUAGE MultiWayIf #-}
+
 module Day22 where
 
+import Control.Monad (when)
 import Control.Monad.ST (ST, runST)
+import Control.Parallel.Strategies
 import Data.Bits (Bits (..))
+import Data.Function (on)
+import Data.List (foldl')
 import Data.Vector.Unboxed qualified as U
 import Data.Vector.Unboxed.Mutable qualified as M
 import Paths_AOC2024 (getDataDir)
-import Control.Monad (when)
 
 mixPrune f = (.&. (16777216 - 1)) . (\x -> xor x (f x)) :: Int -> Int
 
 step = mixPrune (`shiftL` 11) . mixPrune (`shiftR` 5) . mixPrune (`shiftL` 6)
 
 toIM :: [Int] -> Int
-toIM = foldr (\x acc -> acc * 20 + (x + 9)) 0
+toIM = foldl' (\acc x -> acc * 20 + (x + 9)) 0
 
-solveBST' time input@(x : xs) =  runST (M.replicate (20 ^ 4) (0 :: Int) >>= go (time - 4) 0 1 ds x5 xs)
+solveBST' time input@(x : xs) = runST (M.replicate (20 ^ 4) (0, 0) >>= go (time - 4) 0 1 (toIM ds) x5 xs)
   where
     fds = ((,) <$> (zipWith subtract <$> map (`mod` 10) <*> map (`mod` 10) . tail) <*> last) . take 5 . iterate step
     (ds, x5) = fds x
     n = length input
-    go :: Int -> Int -> Int -> [Int] -> Int -> [Int] -> M.STVector s Int -> ST s (Int, Int)
-    go !t !s !round ds@[!d0, !d1, !d2, !d3] !x xs v = do
-      -- traceM $ show (ds, x)
-      (m0, round0) <- (`divMod` n) <$> M.read v i
+    go :: Int -> Int -> Int -> Int -> Int -> [Int] -> M.STVector s (Int, Int) -> ST s (Int, Int)
+    go !t !s !round ds0 !x xs v = do
+      (m0, round0) <- M.read v ds0
       let m1 = m0 + (x `mod` 10)
-      when (round0 /= round) $ M.write v i (round + m1 * n)
-      if | t <= 0, [] <- xs -> (s', ) <$> M.foldl' (\acc y -> max acc (y `div` n)) 0 v
-         | t <= 0, y : xs1 <- xs, (ds1, x1) <- fds y -> go (time - 4) s' (succ round) ds1 x1 xs1 v
-         | otherwise -> go (pred t) s round ds' x' xs v
+      when (round0 /= round) $ M.write v ds0 (m1, round)
+      if
+        | t <= 0, [] <- xs -> (s',) <$> M.foldl' (\acc y -> max acc (fst y)) 0 v
+        | t <= 0, y : xs1 <- xs, (ds1, x1) <- fds y -> go (time - 4) s' (succ round) (toIM ds1) x1 xs1 v
+        | otherwise -> go (pred t) s round ds' x' xs v
       where
-        i = toIM ds
         s' = s + x
         x' = step x
-        ds' = [d1, d2, d3, (x' `mod` 10) - (x `mod` 10)]
-
-solveBST input = runST (M.replicate (20 ^ 4) (0 :: Int) >>= go 1 (map (map (`mod` 10)) input))
-  where
-    n = length input
-    go :: Int -> [[Int]] -> M.STVector s Int -> ST s Int
-    go round [] v = M.foldl' (\acc x -> max acc (x `div` n)) 0 v
-    go round ((a : b : c : d : e : xs) : ys) v = do
-      let i = toIM [b - a, c - b, d - c, e - d]
-      (m0, round0) <- (`divMod` n) <$> M.read v i
-      let m1 = m0 + e
-      if round0 /= round
-        then M.write v i (round + m1 * n) >> go round ((b : c : d : e : xs) : ys) v
-        else go round ((b : c : d : e : xs) : ys) v
-    go round (_ : ys) v = go (round + 1) ys v
+        ds' = ((ds0 `mod` (20 ^ 3)) * 20) + (x' `mod` 10) - (x `mod` 10) + 9
 
 day22 :: IO ()
 day22 = do
   input <- map (read @Int) . lines <$> (readFile . (++ "/input/input22.txt") =<< getDataDir)
-  -- input <- map (read @Int) . lines <$> (readFile . (++ "/input/test22.txt") =<< getDataDir)
   let (ansA, ansB) = solveBST' 2000 input
   putStrLn
     . ("day22a: " ++)
