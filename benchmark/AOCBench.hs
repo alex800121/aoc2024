500167fdead498f8acdae8a41ecc480c409982ab
alex800121 <alex800121@hotmail.com>
2025-12-08 16:29:31 +0800

    show day results in bench

--- benchmark/AOCBench.hs
+++ benchmark/AOCBench.hs
@@ -33,34 +33,34 @@ import Solve (solveAll)
 main :: IO ()
 main =
   defaultMainWith
-    (defaultConfig {verbosity = Quiet, reportFile = Just "./bench.html"})
-    [ bench "main" (nfIO solveAll),
-      bgroup
+    (defaultConfig{verbosity = Quiet, reportFile = Just "./bench.html"})
+    [ bench "main" (nfIO solveAll)
+    , bgroup
         "Day"
-        [ bench "day1" (nfIO day1),
-          bench "day2" (nfIO day2),
-          bench "day3" (nfIO day3),
-          bench "day4" (nfIO day4),
-          bench "day5" (nfIO day5),
-          bench "day6" (nfIO day6),
-          bench "day7" (nfIO day7),
-          bench "day8" (nfIO day8),
-          bench "day9" (nfIO day9),
-          bench "day10" (nfIO day10),
-          bench "day11" (nfIO day11),
-          bench "day12" (nfIO day12),
-          bench "day13" (nfIO day13),
-          bench "day14" (nfIO day14),
-          bench "day15" (nfIO day15),
-          bench "day16" (nfIO day16),
-          bench "day17" (nfIO day17),
-          bench "day18" (nfIO day18),
-          bench "day19" (nfIO day19),
-          bench "day20" (nfIO day20),
-          bench "day21" (nfIO day21),
-          bench "day22" (nfIO day22),
-          bench "day23" (nfIO day23),
-          bench "day24" (nfIO day24),
-          bench "day25" (nfIO day25)
+        [ bench "day1" (nfIO (day1 >>= print))
+        , bench "day2" (nfIO (day2 >>= print))
+        , bench "day3" (nfIO (day3 >>= print))
+        , bench "day4" (nfIO (day4 >>= print))
+        , bench "day5" (nfIO (day5 >>= print))
+        , bench "day6" (nfIO (day6 >>= print))
+        , bench "day7" (nfIO (day7 >>= print))
+        , bench "day8" (nfIO (day8 >>= print))
+        , bench "day9" (nfIO (day9 >>= print))
+        , bench "day10" (nfIO (day10 >>= print))
+        , bench "day11" (nfIO (day11 >>= print))
+        , bench "day12" (nfIO (day12 >>= print))
+        , bench "day13" (nfIO (day13 >>= print))
+        , bench "day14" (nfIO (day14 >>= print))
+        , bench "day15" (nfIO (day15 >>= print))
+        , bench "day16" (nfIO (day16 >>= print))
+        , bench "day17" (nfIO (day17 >>= print))
+        , bench "day18" (nfIO (day18 >>= print))
+        , bench "day19" (nfIO (day19 >>= print))
+        , bench "day20" (nfIO (day20 >>= print))
+        , bench "day21" (nfIO (day21 >>= print))
+        , bench "day22" (nfIO (day22 >>= print))
+        , bench "day23" (nfIO (day23 >>= print))
+        , bench "day24" (nfIO (day24 >>= print))
+        , bench "day25" (nfIO (day25 >>= print))
         ]
     ]
